e806b6795d66a9a39ad2323b3d7ecd3a82465ad2
jaworskimateusz <jaworskimateuszm@gmail.com>
2018-11-25 16:53:07 +0100

    Configure Data Source

--- spring-security-base-app/src/main/java/com/springsecurity/config/ApplicationConfig.java
+++ spring-security-base-app/src/main/java/com/springsecurity/config/ApplicationConfig.java
@@ -1,19 +1,56 @@
 package com.springsecurity.config;
 
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.springsecurity")
+@PropertySource("classpath:persistence-mysql.properties")
 public class ApplicationConfig {
 
-	//define a bean for ViewResolver
+	//set up variable to hold the properties, env hold data from properites files, helper class
+	@Autowired
+	private Environment env;
 	
+	//set up a logger for diagnostics
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
+	//define a bean for our security datasource
+	@Bean
+	public DataSource securityDataSourve() {
+		//create connection pool
+		ComboPooledDataSource securityDataSource
+			= new ComboPooledDataSource();
+		
+		//set the JDBC driver class
+		try {
+			securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+		} catch (PropertyVetoException e) {
+			throw new RuntimeException(e);
+		}
+		
+		logger.info(">> jdbc.url: " + env.getProperty("jdbc.url"));
+		logger.info(">> jdbc.user: " + env.getProperty("jdbc.user"));
+		//set connection pool 
+		return securityDataSource;
+	}
+	
+	//define a bean for ViewResolver
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
