fd6bd75638d45bdbda2f1a036bff9bcaf92e6d57
jaworskimateusz <jaworskimateuszm@gmail.com>
2018-11-26 18:19:16 +0100

    Run SQL script and connect with database

--- spring-security-base-app/src/main/java/com/springsecurity/config/ApplicationConfig.java
+++ spring-security-base-app/src/main/java/com/springsecurity/config/ApplicationConfig.java
@@ -1,19 +1,47 @@
 package com.springsecurity.config;
 
+import java.beans.PropertyVetoException;
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.springsecurity")
+@PropertySource("classpath:persistence-mysql.properties")
 public class ApplicationConfig {
 
-	//define a bean for ViewResolver
+	@Autowired
+	private Environment env;
 	
+	private Logger logger = Logger.getLogger(getClass().getName());
+	
+	@Bean
+	public DataSource securityDatasource() {
+		ComboPooledDataSource securityDataSource
+			= new ComboPooledDataSource();
+		try {
+			securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+		} catch(PropertyVetoException e) {
+			throw new RuntimeException(e);
+		}
+		logger.info(">> JDBC user is: " + env.getProperty("jdbc.user"));
+		return securityDataSource;
+	}
+	
+	//define a bean for ViewResolver
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
